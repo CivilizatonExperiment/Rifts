18b43b85771702fc0903aa1ee035bef6e5c233f5
Sharpcastle33 <sharpcastle33@gmail.com>
2019-03-18 17:48:16 -0400

    more gui work

--- src/main/java/com/gmail/sharpcastle33/listeners/ExtractorCreationListener.java
+++ src/main/java/com/gmail/sharpcastle33/listeners/ExtractorCreationListener.java
@@ -1,8 +1,11 @@
 package com.gmail.sharpcastle33.listeners;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.ShulkerBox;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -16,20 +19,24 @@ public class ExtractorCreationListener implements Listener {
   
   @EventHandler
   public void createExtractor(BlockPlaceEvent event){
-    
+    Player p = event.getPlayer();
     ItemStack i = event.getItemInHand();
     if(i.hasItemMeta()
         && i.getItemMeta().hasDisplayName()
         && i.getItemMeta().getDisplayName().equals(EXTRACTOR_ITEM_NAME)) {
       Block b = event.getBlock();
-      if(b.getType() == Material.BLACK_SHULKER_BOX) {
+      //valid block and location
+      if(b.getType() == Material.BLACK_SHULKER_BOX
+          && isValidExtractorLocation(b.getLocation())) {
         
         ShulkerBox box = (ShulkerBox) b.getState();
         Inventory inv = box.getInventory();
         inv.clear();
         
         
-        
+       //invalid location 
+      }else {
+        p.sendMessage(ChatColor.RED + "Void Extractors must be placed on top of furnaces, which are used as a fuel source.");
       }
       
       
@@ -40,5 +47,12 @@ public class ExtractorCreationListener implements Listener {
     }
     
   }
+  
+  public Boolean isValidExtractorLocation(Location loc) {
+    if(loc.getBlock().getRelative(BlockFace.DOWN).getType() == Material.FURNACE) {
+      return true;
+    }else return false;
+  }
+ 
 
 }
--- src/main/java/com/gmail/sharpcastle33/listeners/ExtractorGUI.java
+++ src/main/java/com/gmail/sharpcastle33/listeners/ExtractorGUI.java
@@ -1,17 +1,19 @@
 package com.gmail.sharpcastle33.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import net.md_5.bungee.api.ChatColor;
 
 public class ExtractorGUI implements Listener{
   
   public static String EXTRACTOR_GUI_NAME = ChatColor.DARK_BLUE + "Void Extractor";
-  public static String BUTTON_UNANCHOR = ChatColor.GOLD + "Unanchor Extractor";
-  public static String BUTTON_ANCHOR = ChatColor.GOLD + "Anchor and Activate";
+  public static String BUTTON_UNANCHOR = ChatColor.RED + "Unanchor Extractor";
+  public static String BUTTON_ANCHOR = ChatColor.GREEN + "Anchor and Activate";
   public static String INFORMATION = ChatColor.BLUE + "Information";
   public static String RIFT_INFORMATION = ChatColor.BLUE + "Rift Information";
   
@@ -34,6 +36,42 @@ public class ExtractorGUI implements Listener{
       if(!(invName.equals(EXTRACTOR_GUI_NAME))) {
         return;
       }
+      
+      if (clicked.getType() == Material.ENDER_PEARL) {
+        event.setCancelled(true);
+        p.sendMessage(ChatColor.RED + "Enderpearls may not be put in this device");
+      }
+      
+      if(event.getClickedInventory().getName().equals(EXTRACTOR_GUI_NAME)) {
+        
+      }
+      
+      if(event.getClickedInventory() instanceof PlayerInventory){
+        
+      }
+      
+      if (clicked.hasItemMeta() && clicked.getItemMeta().hasDisplayName()
+          && clicked.getItemMeta().getDisplayName().equals(ChatColor.RED + "")) {
+      event.setCancelled(true);
+      }
+      
+      if (clicked.hasItemMeta() && clicked.getItemMeta().hasDisplayName()
+          && clicked.getItemMeta().getDisplayName().equals(BUTTON_ANCHOR)) {
+        
+        
+        
+        
+      }
+      
+      if (clicked.hasItemMeta() && clicked.getItemMeta().hasDisplayName()
+          && clicked.getItemMeta().getDisplayName().equals(BUTTON_UNANCHOR)) {
+        
+        
+        
+        
+        
+      }
+      
       
       //Check if the extractor is anchored
       
